48f948d29384e1df4ef8ed046eab0153d03ebe07
dodonator <dodo@chaosdorf.de>
2022-12-01 19:52:32 +0100

    a new version
    a new solution using functional programming

--- 2022-12-01.py
+++ 2022-12-01.py
@@ -1,39 +1,36 @@
 # https://adventofcode.com/2022/day/1
 from pathlib import Path
-from typing import List
+from typing import Iterator, List
+from pprint import pprint as print
+
+
+def split_list(p_list: List, separator) -> Iterator[List]:
+    while separator in p_list:
+        ind = p_list.index(separator)
+        part = p_list[:ind]
+        if part:
+            yield part
+        p_list = p_list[ind + 1 :]
+
+    if p_list:
+        yield p_list
+
 
 filename = "2022-12-01_input.txt"
 filepath = Path(filename)
 
-elves: List[int] = []
-
 with filepath.open("r") as stream:
-    current_total: int = 0  # current calorie total
-    elf_counter = 0  # index of the current elf
+    # strip "\n" from each line
+    lines = [line.strip("\n") for line in stream.readlines()]
 
-    for line in stream:
-        line = line.strip("\n")
+# convert lines to integer (empty lines become 0)
+values = [int(line) if line else 0 for line in lines]
 
-        if line:
-            # add value to total
-            cal = int(line)
-            print(f"elf {elf_counter:>2}: {cal:>10}")
-            current_total += int(line)
+# split list by the empty lines (zero values)
+elves = list(sorted((sum(part) for part in split_list(values, 0)), reverse=True))
 
-        else:
-            # save total to list
-            elves.append(current_total)
-            current_total = 0
-            elf_counter += 1
-    else:
-        # add the last total
-        elves.append(current_total)
+# part 1
+print(elves[0])  # in my case 73211
 
-# sort the elves by their calorie total descending
-elves.sort(reverse=True)
-
-# answer part 1
-print(max(elves))
-
-# answer part 2
-print(sum(elves[:3]))
+# part 2
+print(sum(elves[:3]))  # in my case 213958
