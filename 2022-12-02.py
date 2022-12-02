bef00ee9476191567c559816a10e3e13ef6189cf
dodonator <dodo@chaosdorf.de>
2022-12-02 13:40:28 +0100

    day2: solution for second star
    needs some refactoring

--- 2022-12-02.py
+++ 2022-12-02.py
@@ -1,28 +1,22 @@
 # https://adventofcode.com/2022/day/2
 from pathlib import Path
 
-CHOICES = {
-    "A": "ROCK",
-    "B": "PAPER",
-    "C": "SCISSORS",
-    "X": "ROCK",
-    "Y": "PAPER",
-    "Z": "SCISSORS",
+TABLE = {
+    "A": "rock",
+    "B": "paper",
+    "C": "scissors",
+    "X": "lose",
+    "Y": "draw",
+    "Z": "win",
 }
 
-SCORES = {"ROCK": 1, "PAPER": 2, "SCISSOR": 3}
+MOVES = [
+    {"rock": "scissors", "paper": "rock", "scissors": "paper"},  # key wins to value
+    {"rock": "rock", "paper": "paper", "scissors": "scissors"},  # key draws to value
+    {"rock": "paper", "paper": "scissors", "scissors": "rock"},  # key loses to value
+]
 
-RESULTS = {
-    ("ROCK", "ROCK"): 4,
-    ("ROCK", "PAPER"): 8,
-    ("ROCK", "SCISSORS"): 3,
-    ("PAPER", "ROCK"): 1,
-    ("PAPER", "PAPER"): 5,
-    ("PAPER", "SCISSORS"): 9,
-    ("SCISSORS", "ROCK"): 7,
-    ("SCISSORS", "PAPER"): 2,
-    ("SCISSORS", "SCISSORS"): 6,
-}
+SCORES = {"rock": 1, "paper": 2, "scissors": 3, "lose": 0, "draw": 3, "win": 6}
 
 filepath = Path(f"input_{Path(__file__).stem}.txt")
 
@@ -31,14 +25,23 @@ with filepath.open("r") as stream:
     for line in stream:
         line = line.strip("\n")
 
-        tmp = line.split(" ")
-        enemy_choice = CHOICES.get(tmp[0])
-        my_choice = CHOICES.get(tmp[1])
+        score = 0
+        opponent_move_str = TABLE.get(line.split(" ")[0])
+        desired_result_str = TABLE.get(line.split(" ")[1])
 
-        result = (enemy_choice, my_choice)
-        score = RESULTS.get(result)
+        result_score = SCORES.get(desired_result_str)
+        tmp_index = result_score // 3
+
+        needed_move = MOVES[tmp_index].get(opponent_move_str)
+        move_score = SCORES.get(needed_move)
+
+        score = result_score + move_score
         total_score += score
-        print(result, score)
 
-# solution part 1
-print(total_score)  # in my case 12855
+        print(
+            f"my opponent will use {opponent_move_str} i am supposed to {desired_result_str}"
+        )
+        print(f"so i must choose {needed_move}")
+
+# solution for part 2
+print(total_score)  # in my case 13726
