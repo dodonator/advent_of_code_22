f8a1a9187a157d7091cf8ea1fcf5f40b16192af7
dodonator <dodo@chaosdorf.de>
2022-12-03 11:33:31 +0100

    day 3: put parts in two functions

--- 2022-12-03.py
+++ 2022-12-03.py
@@ -2,27 +2,39 @@
 
 from pathlib import Path
 from string import ascii_uppercase, ascii_lowercase
+from more_itertools import grouper
 
 chars = "#" + ascii_lowercase + ascii_uppercase
 
 filename = f"input_" + Path(__file__).stem + ".txt"
 filepath = Path(filename)
 
-priority_sum = 0
-with filepath.open("r") as stream:
-    for line in stream:
-        line = line.strip("\n")
-        # get number of items
-        item_number = len(line)
-        # split line at midpoint
-        midpoint = item_number // 2
-        first, second = set(line[:midpoint]), set(line[midpoint:])
-        # get intersection of compartments
-        common = set.intersection(first, second).pop()
-        priority = chars.index(common)
-        print(priority)
-        # add up priorities
-        priority_sum += priority
 
-# solution part 1
-print(priority_sum)  # in my case 7826
+def part_one():
+    priority_sum = 0
+    with filepath.open("r") as stream:
+        for line in stream:
+            line = line.strip("\n")
+            # get number of items
+            item_number = len(line)
+            # split line at midpoint
+            midpoint = item_number // 2
+            first, second = set(line[:midpoint]), set(line[midpoint:])
+            # get intersection of compartments
+            common = set.intersection(first, second).pop()
+            priority = chars.index(common)
+            print(priority)
+            # add up priorities
+            priority_sum += priority
+
+    # solution part 1
+    return priority_sum  # in my case 7826
+
+
+def part_two():
+    with filepath.open("r") as stream:
+        lines = (line.strip("\n") for line in stream.readlines())
+
+    groups = grouper(lines, 3)
+    for g in groups:
+        pass
