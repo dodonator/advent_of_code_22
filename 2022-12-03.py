00ce5474a186f2a053b71cf9e00dc058072d41d5
dodonator <dodo@chaosdorf.de>
2022-12-03 11:23:07 +0100

    day 3: first solution for part 1

---
+++ 2022-12-03.py
@@ -0,0 +1,24 @@
+# https://adventofcode.com/2022/day/3
+
+from pathlib import Path
+from string import ascii_uppercase, ascii_lowercase
+
+chars = "#" + ascii_lowercase + ascii_uppercase
+
+filename = f"input_" + Path(__file__).stem + ".txt"
+filepath = Path(filename)
+
+priority_sum = 0
+with filepath.open("r") as stream:
+    for line in stream:
+        line = line.strip("\n")
+        item_number = len(line)
+        midpoint = item_number // 2
+        first, second = set(line[:midpoint]), set(line[midpoint:])
+        common = set.intersection(first, second).pop()
+        priority = chars.index(common)
+        print(priority)
+        priority_sum += priority
+
+# solution part 1
+print(priority_sum)  # in my case 7826
