f0d44646810c1b91815f58109ea1ec899a6c4477
dodonator <dodo@chaosdorf.de>
2022-12-04 15:33:07 +0100

    day 4: solution for part 2

--- 2022-12-04.py
+++ 2022-12-04.py
@@ -6,7 +6,8 @@ filename = f"input_" + Path(__file__).stem + ".txt"
 filepath = Path("2022") / Path(filename)
 
 with filepath.open("r") as stream:
-    overlap_counter = 0
+    complete_overlap = 0
+    overlap = 0
     for line in stream:
         line = line.strip("\n")
 
@@ -15,10 +16,19 @@ with filepath.open("r") as stream:
         low_2, high_2 = tuple(map(int, pair[1].split("-")))
 
         if low_1 >= low_2 and high_1 <= high_2:
-            overlap_counter += 1
+            complete_overlap += 1
         elif low_2 >= low_1 and high_2 <= high_1:
-            overlap_counter += 1
-        else:
-            pass
+            complete_overlap += 1
+        elif low_1 >= low_2 and low_1 <= high_2:
+            overlap += 1
+        elif low_2 >= low_1 and low_2 <= high_1:
+            overlap += 1
+
+    overlap += complete_overlap
+
+
 # solution part 1
-print(overlap_counter)  # in my case 573
+print(complete_overlap)  # in my case 573
+
+# solution part 2
+print(overlap)  # in my case 294
