c10af15ec276bd119444e5b0d6c1daf57d99806a
dodonator <dodo@chaosdorf.de>
2022-12-09 19:14:45 +0100

    renamed files

---
+++ day-08.py
@@ -0,0 +1,10 @@
+# https://adventofcode.com/2022/day/8
+
+from pathlib import Path
+
+aoc_folder = Path.cwd()
+input_file = "input_day-08.txt"
+filepath = aoc_folder / Path("2022") / Path(input_file)
+
+with filepath.open("r") as stream:
+    pass
